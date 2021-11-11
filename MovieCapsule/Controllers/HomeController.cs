021d08223ae247b1c2f1bdbcd29499e4da5634a7
Melis Çoban <melis.coban5@gmail.com>
2021-11-11 13:30:59 +0300

    Add project files.

---
+++ MovieCapsule/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MovieCapsule.Models;
+using MovieCapsule.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieCapsule.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public HomeController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var vm = new HomeViewModel()
+            {
+                Genres = _context.Genres.Select(x => new SelectListItem() { Text = x.Name, Value = x.Id.ToString() }).ToList(),
+                Movies = _context.Movies.Include(x => x.Genres).ToList()
+            };
+
+            return View(vm);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MovieCapsule/ViewModels/HomeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using MovieCapsule.Models;
+using MovieCapsule.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieCapsule.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Movie> Movies { get; set; }
+
+        public List<SelectListItem> Genres { get; set; }
+    }
+}
